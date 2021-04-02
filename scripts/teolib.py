74d641171c5182e77c8ee18424c43c01aed1a06d
tjrb <teob18@gmail.com>
2021-04-02 13:49:10 +0100

    update license

--- scripts/teolib.py
+++ scripts/teolib.py
@@ -1,3 +1,18 @@
+"""
+    teolib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    teolib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with teolib.  If not, see <https://www.gnu.org/licenses/>6.
+"""
+
 
 class sets:
     def __init__(self,cam=None,net=None,classnames=None,classcolors=None,layers=None):
